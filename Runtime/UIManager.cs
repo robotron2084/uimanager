6a676a0f5c110a9f9c2ddada8a0dbe50200ddf94
Chris Hill <chris@enemyhideout.com>
2023-01-20 21:21:59 -0700

    Basic package setup.

---
+++ Runtime/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace com.enemyhideout.ui
+{
+  public class UIManager
+  {
+    public Canvas Root;
+    public Camera Cam;
+    
+    public void ShowScreen()
+    {
+      
+    }
+  }
+}
