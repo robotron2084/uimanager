8f43febecec142204613d23e0cf52ce2fa083479
Chris Hill <chris@enemyhideout.com>
2023-01-22 21:45:16 -0700

    Adding sample code.

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ A library for managing screens of UI for unity
 Features
 
 Display screens in a modal, with a shield.
+Confirm Cancel Modal
 
 TODO
+Some bugs with the screen stack to work out.
+Add ability for listeners to cancel out the closing of a modal.
+Don't hide modals when a new modal shows? Make that configurable?
---
+++ Samples~/UIManager.BasicExample/Code/UIManagerDemo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.enemyhideout.ui.example
+{
+  public class UIManagerDemo : MonoBehaviour
+  {
+    [SerializeField]
+    private UIManager _uiManager;
+
+    [SerializeField] private UIScreen WelcomePrefab;
+    [SerializeField] private UIScreen SelectionPrefab;
+
+
+    private IEnumerator Start()
+    {
+      // give unity a sec to catch up so we can see animations effectively.
+      yield return new WaitForSeconds(1.0f);
+      
+      //create an instance of the welcome screen.
+      var welcomeScreen = _uiManager.ShowPrefab(WelcomePrefab);
+      // wait for it to close
+      yield return welcomeScreen.WaitForClose();
+      // we're done with it, let's go ahead and clean it up.
+      welcomeScreen.Despawn();
+      
+      var selectionScreen = _uiManager.ShowPrefab(SelectionPrefab);
+      while (true)
+      {
+        yield return selectionScreen.WaitForResult();
+        //based upon the result, lets's show another ui.
+        var subScreen = ScreenForResult(selectionScreen.Result);
+        yield return subScreen.WaitForResult();
+        var confirm =_uiManager.ShowConfirm(null, $"Your Choice was {subScreen.Result}");
+        subScreen.Despawn();
+        yield return confirm.WaitForClose();
+      }
+
+    }
+
+    private UIScreen ScreenForResult(int selectionScreenResult)
+    {
+      switch (selectionScreenResult)
+      {
+        case UIScreen.Confirm:
+          return _uiManager.ShowConfirmCancel("This is the title.", "This is the message.");
+      }
+
+      return null;
+    }
+  }
+}
